caf032c0998271494ebed80700abcb2e322f6a34
Estêvan <esteevan160@gmail.com>
2025-02-12 20:18:25 -0300

    lista de exercicio 1 exercicio 2

---
+++ src/exercicio2lista1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class exercicio2lista1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Qual é o seu nome?: ");
+        String NOME = sc.next();
+        System.out.print("Olá "+NOME+", é um prazer te conhecer!");
+
+
+    }
+
+}
+//Qual é o seu nome? João da Silva
+//Olá João da Silva, é um prazer te conhecer!
