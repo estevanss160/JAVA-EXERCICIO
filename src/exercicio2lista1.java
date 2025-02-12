b6635ac17a91c1051bac1b46442669a438eb6e58
Estêvan <esteevan160@gmail.com>
2025-02-12 20:35:45 -0300

    lista de exercicio 1 exercicio 2

--- src/exercicio2lista1.java
+++ src/exercicio2lista1.java
@@ -4,7 +4,11 @@ public class exercicio2lista1 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Qual é o seu nome?: ");
         String NOME = sc.next();
-        System.out.print("Olá "+NOME+", é um prazer te conhecer!");
+        System.out.print("qual a sua idade: ");
+        int idade = sc.nextInt();
+
+        System.out.println("Olá "+NOME+", é um prazer te conhecer!");
+        System.out.print("Sua idade é: "+idade);
 
 
     }
@@ -12,3 +16,4 @@ public class exercicio2lista1 {
 }
 //Qual é o seu nome? João da Silva
 //Olá João da Silva, é um prazer te conhecer!
+//sua idade é:
