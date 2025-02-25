9d87a56e37e92264bfedadc6dd44518e089997b6
Mikeias Lopes <mikeiaslopes10@gmail.com>
2025-02-25 19:09:51 -0300

    Modifiquei o ex16

--- src/exercicio16lista1.java
+++ src/exercicio16lista1.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class exercicio16lista1 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Quantos cigarros você fuma por dia? ");
+        System.out.print("Quantos cigarros você fuma por dia atualmente? ");
         int qtd_cigarros = teclado.nextInt();
         System.out.print("Quantos anos você fuma? ");
         float qtd_anos = teclado.nextFloat();
